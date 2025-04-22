2a6cbf00b1e2642c0ca729f5ebe8e467043fbb18
TheGeekosaurus <58343707+TheGeekosaurus@users.noreply.github.com>
2025-04-21 20:49:59 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: './src/popup/index.tsx',
+    background: './src/background.ts',
+    contentScript: './src/contentScript.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/popup/popup.html',
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public/manifest.json', to: 'manifest.json' },
+        { from: './public/icons', to: 'icons' },
+      ],
+    }),
+  ],
+};
