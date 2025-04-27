cc706f496832e46beb1a0f202884e1c266882b5b
TheGeekosaurus <58343707+TheGeekosaurus@users.noreply.github.com>
2025-04-26 17:30:05 -0700

    Update amazon.ts

--- src/content/selectors/amazon.ts
+++ src/content/selectors/amazon.ts
@@ -6,29 +6,38 @@
 export const amazonSelectors = {
   title: [
     '#productTitle',
-    '.product-title-word-break'
+    '.product-title-word-break',
+    'h1'
   ],
   price: [
     '.a-price .a-offscreen',
     '#priceblock_ourprice',
     '#priceblock_dealprice',
-    '.a-price .a-price-whole'
+    '.a-price .a-price-whole',
+    '.priceToPay .a-offscreen',
+    'span.a-price span.a-offscreen',
+    '.a-section .a-price .a-offscreen'
   ],
   brand: [
     '#bylineInfo',
     '.a-link-normal.contributorNameID',
-    '.po-brand .a-span9'
+    '.po-brand .a-span9',
+    '#productDetails_techSpec_section_1 tr:contains("Brand") td',
+    '#brand'
   ],
   image: [
     '#landingImage',
     '#imgBlkFront',
-    '#main-image-container img'
+    '#main-image-container img',
+    '#imgTagWrapperId img'
   ],
   // Selectors for product details section where UPC/EAN might be found
   details: [
     '.prodDetTable tr',
     '.detail-bullet-list span',
-    '#detailBullets_feature_div li'
+    '#detailBullets_feature_div li',
+    '#productDetails_techSpec_section_1 tr',
+    '#productDetails_detailBullets_sections1 tr'
   ]
 };
 
@@ -37,8 +46,8 @@ export const amazonSelectors = {
  */
 export const amazonRegexPatterns = {
   asin: [
-    /\/dp\/([A-Z0-9]{10})/,
-    /\/gp\/product\/([A-Z0-9]{10})/
+    /\/dp\/([A-Z0-9]{10})/i,
+    /\/gp\/product\/([A-Z0-9]{10})/i
   ],
   upc: /\d{12,13}/
 };
